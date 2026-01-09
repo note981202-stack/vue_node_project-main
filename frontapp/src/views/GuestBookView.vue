0b46ff116aeda1fd0f0cbc6e2a6089737dc6e73e
note981202-stack <note981202@gmail.com>
2026-01-09 15:38:03 +0900

    deploy6

--- frontapp/src/views/GuestBookView.vue
+++ frontapp/src/views/GuestBookView.vue
@@ -11,7 +11,7 @@
         class="position-absolute top-50 start-50 translate-middle text-info-emphasis"
       >
         <h1 class="fw-bold">📖 Guest Book</h1>
-        <p>테스트 중입니다!!!!! 감사합니다</p>
+        <p>테스트 중입니다2222!!!!! 감사합니다</p>
       </div>
     </div>
 
